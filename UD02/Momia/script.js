5579d16cc5ebebde92215f5479223d4e47597665
la1314 <tsue00@gmail.com>
2019-10-17 17:25:42 +0200

    Añadida nuevas imagenes, falla la función de fenerar aleatoriamente los objetos

--- UD02/Momia/script.js
+++ UD02/Momia/script.js
@@ -6,67 +6,83 @@ window.onload = function () {
     let col = 23
     let classPasillo = "pasillo";
     let classBorde = "borde";
-    let classRectangulo = "rectangulo";
-   
+    let vectorObjetos = new Array();
 
-    
+
     for (let f = 0; f < fila; f++) {
         for (let c = 0; c < col; c++) {
-            if (c == 0 || f == 0 || c == col-1 || f == fila-1 || f == 1 ) {
-                addNodos(f,c, classBorde);
+            if (c == 0 || f == 0 || c == col - 1 || f == fila - 1 || f == 1) {
+                addNodos(f, c, classBorde);
             } else {
-                addNodos(f,c, classPasillo);
+                addNodos(f, c, classPasillo);
             }
         }
-        
+
     }
 
-    agregarRectangulos(4,5,classRectangulo, classPasillo);
-   
-   
+    vectorObjetos = agregarRectangulos(4, 5, vectorObjetos);
+    generarObjetos(vectorObjetos);
+
+
 }
 
-function addNodos(fila, col, clase){
+function addNodos(fila, col, clase) {
     var nodo = document.createElement("div");
-    nodo.id = fila+"-"+col;
+    nodo.id = fila + "-" + col;
     //Ver posición de cada celda
-    //nodo.innerText = fila+"-"+col;
+    //nodo.innerText = fila + "-" + col;
     nodo.classList.add(clase);
     document.getElementById("_contenedor").appendChild(nodo);
 
 }
 
-function agregarRectangulos(filas, columnas, clase, claseRetirar){
+function agregarRectangulos(filas, columnas, vector) {
+
 
     let inicioF = 4;
     let inicioC = 3;
-
+    let index = 0;
     for (let fil = 0; fil < filas; fil++) {
-
         for (let col = 0; col < columnas; col++) {
-            
-            propagarRectangulo(inicioF, inicioC, clase, claseRetirar);
-            inicioC+=4; 
+            vector[index] = inicioF + "-" + inicioC;
+            inicioC += 4;
+            index++;
         }
         inicioC = 3;
-        inicioF+=3;
+        inicioF += 3;
     }
+
+    return vector;
 }
 
-function propagarRectangulo(fil, col, clase, claseRetirar){
+function generarObjetos(vector) {
 
-    document.getElementById(fil+"-"+col).classList.remove(claseRetirar);
-    document.getElementById(fil+"-"+(col-1)).classList.remove(claseRetirar);
-    document.getElementById((fil-1)+"-"+(col-1)).classList.remove(claseRetirar);
-    document.getElementById((fil-1)+"-"+col).classList.remove(claseRetirar);
-    document.getElementById((fil-1)+"-"+(col+1)).classList.remove(claseRetirar);
-    document.getElementById(fil+"-"+(col+1)).classList.remove(claseRetirar);
+    let iterandos = vector.length;
+    console.log(vector);
+    let indexRandom = Math.floor(Math.random() * vector.length);
+    let valor = vector[indexRandom].split("-");
+    propagarRectangulo(valor[0], valor[1], "rectangulo", "pasillo");
+    vector.splice(indexRandom, indexRandom);
+    console.log(vector);
 
-    document.getElementById(fil+"-"+col).classList.add(clase);
-    document.getElementById(fil+"-"+(col-1)).classList.add(clase);
-    document.getElementById((fil-1)+"-"+(col-1)).classList.add(clase);
-    document.getElementById((fil-1)+"-"+col).classList.add(clase);
-    document.getElementById((fil-1)+"-"+(col+1)).classList.add(clase);
-    document.getElementById(fil+"-"+(col+1)).classList.add(clase);
+}
+
+function propagarRectangulo(fil, col, clase, claseRetirar) {
+
+    console.log(fil);
+    console.log(col);    
+   /* document.getElementById(fil + "-" + col).classList.remove(claseRetirar);
+    document.getElementById(fil + "-" + (col - 1)).classList.remove(claseRetirar);
+    document.getElementById((fil - 1) + "-" + (col - 1)).classList.remove(claseRetirar);
+    document.getElementById((fil - 1) + "-" + col).classList.remove(claseRetirar);
+    document.getElementById((fil - 1) + "-" + (col + 1)).classList.remove(claseRetirar);
+    document.getElementById(fil + "-" + (col + 1)).classList.remove(claseRetirar);*/
+
+    document.getElementById(fil + "-" + col).classList.add(clase);
+    document.getElementById(fil + "-" + (col - 1)).classList.add(clase);
+    document.getElementById((fil - 1) + "-" + (col - 1)).classList.add(clase);
+    document.getElementById((fil - 1) + "-" + col).classList.add(clase);
+    document.getElementById((fil - 1) + "-" + (col + 1)).classList.add(clase);
+    document.getElementById(fil + "-" + (col + 1)).classList.add(clase);
 
 }

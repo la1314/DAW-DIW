658687a4c0cd8f6466c7ce8fba4b1afb1f764333
la1314 <tsue00@gmail.com>
2019-10-18 18:23:45 +0200

    Añadida la movilidad del personaje, se requiere de refactorización en las funciones que realizan los movimientos

--- UD02/Momia/script.js
+++ UD02/Momia/script.js
@@ -1,22 +1,24 @@
+let classPasillo = "pasillo";
+let classBorde = "borde";
+let classPersonaje = "personaje";
+let classPuerta = "puerta";
+
 window.onload = function () {
 
     /*Se requieren de 16 filas y 23 columnas
     */
     let fila = 16;
     let col = 23
-    let classPasillo = "pasillo";
-    let classBorde = "borde";
-    let classPersonaje = "personaje";
-    let classPuerta = "puerta";
     let vectorObjetos = new Array();
 
     for (let f = 0; f < fila; f++) {
         for (let c = 0; c < col; c++) {
-            
-            if ( f==1 && c == 11) {
-                
-                addNodos(f, c, classPuerta);
-                incluirClase(f, c, classPersonaje);
+
+            if (f == 1 && c == 11) {
+
+                // addNodos(f, c, classPuerta);
+                addNodos(f, c, classPersonaje);
+                //incluirClase(f, c, classPersonaje);
             } else {
                 if (c == 0 || f == 0 || c == col - 1 || f == fila - 1 || f == 1) {
                     addNodos(f, c, classBorde);
@@ -24,19 +26,42 @@ window.onload = function () {
                     addNodos(f, c, classPasillo);
                 }
             }
-            
+
         }
     }
 
     vectorObjetos = agregarRectangulos(4, 5, vectorObjetos);
     generarObjetos(vectorObjetos);
 
+    document.addEventListener("keydown", devolverValorKey, false);
+
+    function devolverValorKey(e) {
+
+        let keyCode = e.key;
+
+        if (keyCode == "ArrowUp") {
+
+            arriba();
+
+        } else if (keyCode == "ArrowDown") {
+
+            abajo();
+
+        } else if (keyCode == "ArrowLeft") {
+
+            izquierda();
+
+        } else if (keyCode == "ArrowRight") {
+            derecha();
+        }
+
+    }
 
 }
 
 /*
 * Crea y añade div al contenedor asignandole a cada uno una ID única
-* las cuales están representadas en posiciones de una matriz
+* las cuales están representadas en separares de una matriz
 */
 function addNodos(fila, col, clase) {
     var nodo = document.createElement("div");
@@ -47,7 +72,7 @@ function addNodos(fila, col, clase) {
     document.getElementById("_contenedor").appendChild(nodo);
 }
 
-function incluirClase(fil, col, clase){
+function incluirClase(fil, col, clase) {
 
     var nodo = document.createElement("img");
     nodo.classList.add(clase);
@@ -56,7 +81,7 @@ function incluirClase(fil, col, clase){
 
 /*
  * Dado el mapa actual del juego pasamos el numemos de objetos que habŕan por fila y columnas
- * y devolvera un vector con las posiciones donde han de estar estos objetos
+ * y devolvera un vector con las separares donde han de estar estos objetos
  */
 
 function agregarRectangulos(filas, columnas, vector) {
@@ -82,11 +107,11 @@ function agregarRectangulos(filas, columnas, vector) {
 }
 
 //Genera los objetos con los que interactua el personaje, através de un vector que contiene
-// las posiciones donde pueden aparecer dichos objetos
+// las separares donde pueden aparecer dichos objetos
 function generarObjetos(vector) {
 
     let iterandos = vector.length;
-    console.log(vector);
+    //console.log(vector);
 
     for (let index = 0; index < iterandos; index++) {
 
@@ -106,7 +131,7 @@ function generarObjetos(vector) {
         let valor = vector[indexRandom].split("-");
         propagarRectangulo(valor[0], parseInt(valor[1]), clase, "pasillo");
         vector = removerItem(vector, vector[indexRandom]);
-        console.log(vector);
+        // console.log(vector);
 
     }
 
@@ -141,3 +166,68 @@ function removerItem(vector, item) {
         return e !== item;
     });
 };
+
+function arriba() {
+    
+    let elemento = document.getElementsByClassName(classPersonaje);
+    let posicion = elemento[0].id;
+    let separar = elemento[0].id.split("-");
+    fila = parseInt(separar[0]) - 1;
+    col = parseInt(separar[1]);
+    identificador = fila + "-" + col;
+
+    comprobarMovimiento(posicion, identificador);
+}
+
+function abajo() {
+
+    let elemento = document.getElementsByClassName(classPersonaje);
+    let posicion = elemento[0].id;
+    let separar = elemento[0].id.split("-");
+    fila = parseInt(separar[0]) + 1;
+    col = parseInt(separar[1]);
+    identificador = fila + "-" + col;
+
+    comprobarMovimiento(posicion, identificador);
+}
+
+function izquierda() {
+    let elemento = document.getElementsByClassName(classPersonaje);
+    let posicion = elemento[0].id;
+    let separar = elemento[0].id.split("-");
+    fila = parseInt(separar[0]);
+    col = parseInt(separar[1]) - 1;
+    identificador = fila + "-" + col;
+    comprobarMovimiento(posicion, identificador);
+}
+
+function derecha() {
+    let elemento = document.getElementsByClassName(classPersonaje);
+    let posicion = elemento[0].id;
+    let separar = elemento[0].id.split("-");
+    fila = parseInt(separar[0]);
+    col = parseInt(separar[1]) + 1;
+    identificador = fila + "-" + col;
+    comprobarMovimiento(posicion, identificador);
+}
+
+function comprobarMovimiento(idPJ, idPas) {
+
+    if (document.getElementById(idPas).className == "pasillo" || document.getElementById(idPas).className == "sueloPisado") {
+
+        intercambiarClases(idPJ, idPas);
+    }
+}
+
+function intercambiarClases(idPj, idPas) {
+
+    divPersonaje = document.getElementById(idPj);
+    divPasillo = document.getElementById(idPas);
+    divPasilloClase = divPasillo.className;
+
+    divPersonaje.classList.remove(classPersonaje);
+    divPasillo.classList.remove(divPasilloClase);
+
+    divPersonaje.classList.add(divPasilloClase);
+    divPasillo.classList.add(classPersonaje);
+}

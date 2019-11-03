c226c713c0d055fa5c7bcdd1cb95e40f98c219bd
la1314 <44264388+la1314@users.noreply.github.com>
2019-11-03 10:47:14 +0100

    Añadidos comentarios para mayor comprensión del código y modificaciones pequeñas realizadas

--- UD02/Practicas/drumkit.js
+++ UD02/Practicas/drumkit.js
@@ -1,30 +1,40 @@
-window.onload = function () {
+window.onload = function() {
 
-    teclas = document.querySelectorAll('.key');
+  //Mediante querySelectorAll se guarda una NodeList dentro de la variable teclas de todos los elementos que tengan la clase 'key'
+  teclas = document.querySelectorAll('.key');
 
-    teclas.forEach(element => element.addEventListener('transitionend', eliminarTransision, false));
+  //Mediante forEach a cada elemento del NodeList se se le añade un EventListener el cual al acabar una transion llamara a la función eliminarTransision()
+  teclas.forEach(element => element.addEventListener('transitionend', eliminarTransision, false));
 
-    window.addEventListener("keydown", llamarSonido);
+  // Se añade un EventListener a toda la ventana del DOM el cual al ser presionada una tecla llamara a la función llamarSonido
+  window.addEventListener('keydown', llamarSonido);
 
-    function llamarSonido(e) {
+}
 
-        const sonido = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//Función encargada de reproducir un sonido cuando un tecla es "pulsada" en la ventana del DOM
+function llamarSonido(e) {
 
+  //En una constante se "intenta" guardar el elemento audio cuyo atributo data-key sea igual al evento keyCode que se ha producido, si este elemento no se encuentra en el HTML el siguiente if retornara la función y no hará nada
+  // por el contrario si este elemento se encuentra en el HTML se procedera con la secuencia de ejecución del código
+  const sonido = document.querySelector(`audio[data-key='${e.keyCode}']`);
 
-        if (!sonido) {
-            return;
-        }
+  // Se guarda el elemento de clase 'key' cuyo atributo data-key sea igual al evento keyCode que se ha producido
+  const transicion = document.querySelector(`.key[data-key='${e.keyCode}']`);
 
-        sonido.currentTime = 0;
-        sonido.play();
-        const transicion = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-        transicion.classList.add('expansion');
-    }
+  if (!sonido) {
+    return;
+  }
 
-    function eliminarTransision() {
+  // La posible reproducción actual del elemento se inicializa a 0
+  sonido.currentTime = 0;
+  // Se procede a reproducir el elemento actual
+  sonido.play();
 
-        this.classList.remove('expansion');
+  //Se añade al elemento key la clase expansion
+  transicion.classList.add('expansion');
+}
 
-    }
-
+//Funcion que elimina la clase 'expansion' del elemento actual
+function eliminarTransision() {
+  this.classList.remove('expansion');
 }

e7da636837ac3f0c59ac12386b812f56b4d9c5a7
la1314 <tsue00@gmail.com>
2019-11-04 16:53:58 +0100

    Añadidos archivos de la Tarea 10

---
+++ UD02/Practicas/Tarea_10/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+
+    let boton = document.querySelector('button');
+
+    boton.addEventListener('click', lanzarEvento);
+
+}
+
+function lanzarEvento(e){
+
+    console.log("pera");
+    
+
+}

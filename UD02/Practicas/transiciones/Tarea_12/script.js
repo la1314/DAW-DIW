f6e39e9dc014ee84091323c72edb189aff706fe4
la1314 <tsue00@gmail.com>
2019-11-08 17:57:58 +0100

    Añadidos cambios pequeños

--- UD02/Practicas/transiciones/Tarea_12/script.js
+++ UD02/Practicas/transiciones/Tarea_12/script.js
@@ -6,6 +6,24 @@
 
 function init(){
     
+    eventoBoton();
+
 }
+
+function eventoBoton(){
+
+    const boton = document.querySelector('button');
+    boton.addEventListener('click', crearCaja);
+
+}
+
+function crearCaja(){
+    
+    let contendor = document.querySelector('container');
+    let caja = document.createElement('box');
+    contendor.appendChild(caja);
+
+}
+
 
 window.onload=init;

d0cb13b5b3feb414ecbf66db129518c7c9b38851
la1314 <44264388+la1314@users.noreply.github.com>
2019-11-09 16:30:44 +0100

    falta implementar mostrar poder y ver si se puede refactorizar

--- UD02/Practicas/transiciones/Tarea_12/script.js
+++ UD02/Practicas/transiciones/Tarea_12/script.js
@@ -1,29 +1,59 @@
-/* 
- 
+/*
+
     ^(;,;)^ : Fragmento perdido
 
 */
 
-function init(){
-    
-    eventoBoton();
+function init() {
+
+  eventoBoton();
+
 
 }
 
-function eventoBoton(){
+function eventoBoton() {
 
-    const boton = document.querySelector('button');
-    boton.addEventListener('click', crearCaja);
-
-}
-
-function crearCaja(){
-    
-    let contendor = document.querySelector('container');
-    let caja = document.createElement('box');
-    contendor.appendChild(caja);
+  const boton = document.querySelector('button');
+  boton.addEventListener('click', crearCaja);
 
 }
 
+function crearCaja() {
 
-window.onload=init;
+  let contendor = document.querySelector('container');
+  let caja = document.createElement('box');
+  caja.addEventListener('click', evolucionar);
+
+  contendor.appendChild(caja);
+
+}
+
+
+function evolucionar() {
+
+  if (!this.className.includes('desevoluciona')) {
+    this.classList.add('evoluciona');
+    this.addEventListener('click', desevolucionar);
+  }
+
+}
+
+function desevolucionar(){
+
+  if (this.className.includes('evoluciona')) {
+    this.classList.add('desevoluciona');
+    this.addEventListener('click', apareceCthu);
+  }
+}
+
+function apareceCthu(){
+  if (this.className.includes('desevoluciona')) {
+    this.classList.add('ultimate');
+  }
+}
+
+function mostrarPoder() {
+  
+}
+
+window.onload = init;

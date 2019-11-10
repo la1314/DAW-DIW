85c92c985e928ab4968653a6d1ecee61446d3587
la1314 <44264388+la1314@users.noreply.github.com>
2019-11-10 18:23:15 +0100

    Añadidas funciones para añadir las clases de girar y recorrer

--- UD02/Practicas/transiciones/Tarea_13/script.js
+++ UD02/Practicas/transiciones/Tarea_13/script.js
@@ -5,17 +5,23 @@ function init() {
 //Añade un evento click al botton para que lanze la funcion crearCaja
 function eventoBoton() {
 
-  const boton = document.querySelector('button');
+  let boton = document.querySelector('button');
   boton.addEventListener('click', crearCaja);
+
+  let divGiro = document.getElementById('_giroO');
+  divGiro.addEventListener('click', ordenGirar);
+
+  let divRecorrer = document.getElementById('_recorridoV');
+  divRecorrer.addEventListener('click', ordenRecorrer);
 
 }
 
 //Al contenedor añade una caja la cual tiene un listener que llamará a la función evoluciona cuando esta sea clickada
 function crearCaja() {
-
+  
+  quitarOrden();
   let contendor = document.querySelector('container');
   let caja = document.createElement('box');
-  caja.classList.add('latente');
   caja.addEventListener('click', evolucionar);
   contendor.appendChild(caja);
 
@@ -39,7 +45,8 @@ function desevolucionar() {
 
   if (this.className.includes('evoluciona')) {
     this.classList.add('desevoluciona');
-    this.addEventListener('click', apareceCthu);
+    this.addEventListener('click', apareceCthu, true);
+
   }
 
 }
@@ -52,7 +59,59 @@ function apareceCthu() {
     this.classList.add("ultimate:hover");
 
   }
-  
+
+}
+
+//Funcion que añade un listener a los seres de Sedefkar que los hará girar si con clickados
+// y añade al resto de cajas el quitar el listener de girar si estás son clickadas
+function ordenGirar() {
+
+  quitarOrden();
+
+  const seres = document.querySelectorAll('.ultimate');
+  seres.forEach(element => element.addEventListener('click', girar));
+
+  let cajas = document.querySelectorAll('box:not(.ultimate)');
+  cajas.forEach(element => element.addEventListener('click', quitarOrden));
+}
+
+function girar() {
+
+  if (this.className.includes('recorrerV')) {
+    this.classList.remove('recorrerV');
+  }
+  this.classList.add('girar');
+  quitarOrden();
+}
+
+//Funcion que añade un listener a los seres de Sedefkar que los hará recorrer en V si con clickados
+// y añade al resto de cajas el quitar el listener de recorrer en V si estás son clickadas
+function ordenRecorrer() {
+  quitarOrden();
+
+  const seres = document.querySelectorAll('.ultimate');
+  seres.forEach(element => element.addEventListener('click', recorrer));
+
+}
+
+function recorrer() {
+  if (this.className.includes('girar')) {
+    this.classList.remove('girar');
+  }
+  this.classList.add('recorrerV');
+  quitarOrden();
+}
+
+//
+function quitarOrden() {
+
+  let cajas = document.querySelectorAll('box');
+  cajas.forEach(element => element.removeEventListener('click', quitarOrden, false));
+
+  const seres = document.querySelectorAll('.ultimate');
+  seres.forEach(element => element.removeEventListener('click', girar, false));
+  seres.forEach(element => element.removeEventListener('click', recorrer, false));
+
 }
 
 window.onload = init;

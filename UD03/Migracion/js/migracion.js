6c3da53981368f4f1bd4eea7be4fa1156f635de9
la1314 <44264388+la1314@users.noreply.github.com>
2019-11-23 19:13:15 +0100

    Añadidas funciones restantes y código comentado, traer al lado bolsa para bomitar por que el código es mierda de la buena

--- UD03/Migracion/js/migracion.js
+++ UD03/Migracion/js/migracion.js
@@ -7,16 +7,34 @@ email : <berlanas_ang@gva.es>
 
 */
 
-/*
+//Muerta el label actual y añade un listener a la barra de progreso siguiente
+function mostrarLabel() {
 
-FUNCIONES PERDIDAS
-^(;,;)^
+  let datas = document.querySelectorAll('steplabel');
 
-*/
+  if (nlabels < datas.length) {
 
+    datas[nlabels].classList.add('estabaEscondido');
+    datas[nlabels].addEventListener('transitionend', mostrarProgreso);
+    nlabels++;
+  }
+}
+
+//Muestra la barra de progreso actual y le añade un listener de transitionend
+function mostrarProgreso() {
+
+  let progresos = document.querySelectorAll('progress');
+  progresos[nprogresos].classList.add('estabaEscondido');
+  progresos[nprogresos].value = 100;
+  progresos[nprogresos].style.width = "5%";
+  progresos[nprogresos].addEventListener('transitionend', sumarProgreso)
+  nprogresos++;
+
+}
+
+//Aumenta de 5 en 5 el tramaño de la anchura del proceso hasta llegar a 95%,
+// entonces remueve el listener del Label y de la barrara de proceso y entonces llama al la función de mostrar mensaje finals
 function sumarProgreso() {
-
-
 
   let anchura = parseInt(this.style.width);
   if (anchura < 95) {
@@ -24,32 +42,48 @@ function sumarProgreso() {
     this.style.width = anchura + '%';
 
     if (anchura >= 95) {
+
+      let datas = document.querySelectorAll('steplabel');
+      datas[nlabels-1].removeEventListener('transitionend', mostrarProgreso);
       this.removeEventListener('transitionend', sumarProgreso);
+      let finals = document.querySelectorAll('finalmsg');
+      mostrarFinal(finals, nfinals);
+      nfinals++;
+
     }
   }
 
+}
+
+//Muestra el nensaje de completado y añade tambien su animación
+// al final llama a la función mostrarLabel para mostrar el siguiente label
+function mostrarFinal(final, posicion) {
+
+  final[posicion].classList.add('estabaEscondido');
+  final[posicion].classList.add('finalmsgAnimation');
+
+  mostrarLabel();
+}
+
+//Elimina el listener del boton y llama a la función mostrarLabel para mostrar el primer elemento label
+function startMigration() {
+
+  document.querySelector("button").removeEventListener("click", startMigration);
+  mostrarLabel();
 
 }
 
-function startMigration(){
+function init() {
 
-    let progresos = document.querySelectorAll('progress');
-    progresos.forEach( function(element){
-      element.value = 100;
-      element.style.width = '10%'
-      element.addEventListener('transitionend', sumarProgreso)
-    });
+  console.info(" * Init envirnoment ");
 
-    // Fragmentos perdidos
-    // ^(;,;)^
-}
+  // Set click function on button
 
-function init(){
-    console.info(" * Init envirnoment ");
-
-    // Set click function on button
-    document.querySelector("button").addEventListener("click",startMigration);
+  document.querySelector("button").addEventListener("click", startMigration);
 }
 
 // Init the environment when all is ready
-window.onload=init;
+window.onload = init;
+let nlabels = 0;
+let nfinals = 0;
+let nprogresos = 0;

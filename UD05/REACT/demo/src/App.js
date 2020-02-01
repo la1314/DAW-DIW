547d9abfb978c663531a8956c32a8ffb6166edb9
la1314 <44264388+la1314@users.noreply.github.com>
2020-02-01 13:51:55 +0100

    REACT

---
+++ UD05/REACT/demo/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Main Annie desde la Beta.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/la1314"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          The Way
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

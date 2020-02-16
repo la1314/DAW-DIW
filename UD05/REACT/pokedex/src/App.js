528ff1845e6543f7a1b34bb6098691e6977e0600
la1314 <44264388+la1314@users.noreply.github.com>
2020-02-16 13:52:07 +0100

    Reorganizando

--- UD05/REACT/pokedex/src/App.js
+++ UD05/REACT/pokedex/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
 import Busqueda from './componentes/Busqueda';
-import Listado from './componentes/Listado';
+
 
 function App() {
 
   return (
     <div className="App">
-      <h1>PokeDex</h1>
       <Busqueda />
-      {/* <Listado /> */}
     </div>
   );
 }
--- UD05/REACT/pokedex/src/componentes/Busqueda.js
+++ UD05/REACT/pokedex/src/componentes/Busqueda.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
+import Listado from './Listado';
 
 export default class Busqueda extends Component {
 
-    /*  constructor(props) {
-         super(props);
-         this.state = {
-             error: null,
-             isLoaded: false,
-             pokemons: [],
-             datos: []
-         };
-     } */
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            allPokemons: [],
+            datosPokemons: [],
+            pokemons: []
+            
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        allPokemons: result.results
+                    });
+
+                    result.results.forEach(element => {
+                        fetch(element.url)
+                            .then(res => res.json()).then(
+                                (result) => {
+                                    this.setState({
+                                        datosPokemons: this.state.datosPokemons.concat(result)
+                                    });
+                                })
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    filtrarPokemons = (e) => {
+        this.setState({
+            pokemons: this.state.datosPokemons.filter(pokemon => pokemon.name.includes(e.target.value))
+        })
+    }
 
     render() {
         return (
-
             <div className="busqueda">
+                <h1>PokeDex</h1>
                 <form id="busquedaform">
-                    <input type="text" name="calle" />
-                    <input type="button" value="Buscar" />
+                    <input type="text" name="calle" onChange={this.filtrarPokemons} />
                 </form>
+                <Listado pokemons={this.state.pokemons} />
+
             </div>
 
         );
